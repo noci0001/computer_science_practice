25a1e2f8ed7c0f2ac5244b84f9f8e7e0831ab0d7
Sam <noci0001@hz.nl>
2022-08-12 11:06:55 +0200

    implementation

--- hangman.c
+++ hangman.c
@@ -13,8 +13,7 @@ int main()
         "purple",
         "windows",
         "linux",
-        "apple"
-    };
+        "apple"};
 
     int randomIndex = rand() % 6;
 
@@ -23,58 +22,100 @@ int main()
     int oldCorrect = 0;
 
     int lengthOfWord = strlen(guessWords[randomIndex]);
-    
+
     //                       0 1 2 3 4 5
     //                       p u r p l e
-    int letterGuessed[8] = { 0,0,0,0,0,0,0,0 };
+    int letterGuessed[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
     int quit = 0;
 
     int loopIndex = 0;
 
     char guess[16]; // where you store the letters you are guessing
-        
+
     char letterEntered;
-
+    
+    // debugger
+    
     printf("guessWords: %s randomIndex: %d lengthOfWord: %d\n",
-            guessWords[randomIndex],
-            randomIndex,
-            lengthOfWord);
-
+           guessWords[randomIndex],
+           randomIndex,
+           lengthOfWord);
+    
     // game loop
 
-    while ( numCorrect < lengthOfWord ) // if word is 7 letter and we guessed only 3, the game continues
+    while (numCorrect < lengthOfWord) // if word is 7 letter and we guessed only 3, the game continues
     {
-        printf("Number correct so far: %d\n", numCorrect);
-        printf("Enter a guess letter:");
-        fgets(guess, 16, stdin);       // method to register the keystroke of the letter you want to guess
+        printf("\n\n New Turn.... \nHangman Word:");
+        
+        for (loopIndex = 0; loopIndex < lengthOfWord; loopIndex++)
+        {
+            if (letterGuessed[loopIndex] == 1) // to ignore already typed letter
+            {
+                printf("%c", guessWords[randomIndex][loopIndex]);
+            } 
+            else {
+                printf("-");
+            }
 
-        if( strncmp(guess, "quit", 4) == 0 )  // checking of the input "quit" in order to break the while loop and exit the game
+
+        }
+
+        printf("\nNumber correct so far: %d\n", numCorrect);
+        printf("\nEnter a guess letter:");
+        fgets(guess, 16, stdin); // method to register the keystroke of the letter you want to guess
+
+        if (strncmp(guess, "quit", 4) == 0) // checking of the input "quit" in order to break the while loop and exit the game
         {
             quit = 1;
             break;
         }
 
-        letterEntered = guess[0]; //get first letter entered
+        letterEntered = guess[0]; // get first letter entered
 
-        scanf("letterGuessed: %d\n", letterGuessed);
-        for ( loopIndex = 0; loopIndex < lengthOfWord; loopIndex++ )
+        scanf("letterEntered: %c\n", &letterEntered);
+
+        oldCorrect = numCorrect;
+
+        for (loopIndex = 0; loopIndex < lengthOfWord; loopIndex++)
         {
-            if(letterGuessed[loopIndex] == 1) {
+            if (letterGuessed[loopIndex] == 1) // to ignore already typed letter
+            {
+                printf("\nYou already typed this letter\n");
                 continue;
             }
-            if( letterEntered == guessWords[randomIndex] [loopIndex] ) {
+            if (letterEntered == guessWords[randomIndex][loopIndex])
+            {
                 letterGuessed[loopIndex] = 1;
                 numCorrect++;
             }
         }
 
+        if( oldCorrect == numCorrect )
+        {
+            numLives--;
+            printf("Sorry, wrong guess\n");
+            if(numLives == 0)
+            {
+                break;
+            }
+        } else
+        {
+            printf("Correct guess :)\n");
+        }
+        
     }
 
-    if (quit == 1) 
+    if (quit == 1)
     {
         printf("\nThe user quit early\n");
+    } else if ( numLives == 0)
+    {
+        printf("Sorry you lose, the word was %s", 
+        guessWords[randomIndex]);
+    } else {
+        printf("Majestic! You guessed the word ' %s '. You won! \n", 
+        guessWords[randomIndex]);
     }
-
     return 0;
 }
