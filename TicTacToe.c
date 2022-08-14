ca1ba120bc1d0e6f1b0eb69ff786798ad561d08c
Sam <noci0001@hz.nl>
2022-08-14 12:08:03 +0200

    Tic Tac Toe first

---
+++ TicTacToe.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/*
+int board[25] = 
+{
+    :,:,:,:,:,
+    :,-,-,-,:,
+    :,-,-,-,;,
+    :,-,-,-,;,
+    :,:,:,:,:,
+}
+*/
+
+// variables declarations 
+
+const NOUGHTS = 1;
+const CROSSES = 2;
+const BORDERS = 3;
+const EMPTY = 0;
+
+const int convertTo25 =
+{
+    6,7,8,
+    11,12,13,
+    16,17,18
+};
+
+void initialiseBoard(int *board)
+{
+    int index = 0;
+
+    for( index=0; index < 25; ++index)
+    {
+
+    }
+};
+
+int main()
+{
+
+    return 0;
+};
